5ef6afc57e90fdf21334e212c6bcd61d91faf5b2
abhii00 <abhijit.pandit808@gmail.com>
2023-03-05 19:40:18 +0000

    removed 0rw and 3rw models, added sensor subsystems, modified reference path for easy testing

--- sartoc/controls/refpath.m
+++ sartoc/controls/refpath.m
@@ -8,8 +8,10 @@ function q_tar = refpath(t, a)
 %Returns:
 %   q_tar (4x1 array): the target quaternion
     
+    a = 2;
+
     switch(a)
-        case 3
+        case 1
             %0 -> PI/4 AROUND YZ WITH 10 STEPS
             N = 10;
             qm_1 = quatconstruct([0, 0, 0], 'rotation', 'matlab');
@@ -22,8 +24,11 @@ function q_tar = refpath(t, a)
                 qm_tar = qms(N);
             end
         case 2
-            qm_tar = quatconstruct(2*pi*t/1000*[0, 1, 1]/norm([0, 1, 1]), 'rotation', 'matlab');
-        case 1
+            %2PI AROUND YZ IN 1000 WITH DELAY -450
+            tm = t - 450;
+            qm_tar = quatconstruct(2*pi*tm/1000*[0, 1, 1]/norm([0, 1, 1]), 'rotation', 'matlab');
+        case 3
+            %0 -> PI/2 AROUND YZ WITH 1 STEP
             qm_tar = quatconstruct(pi/2*[0, 1, 1]/norm([0, 1, 1]), 'rotation', 'matlab');
     end
     q_tar = quatconvert(qm_tar, 'matlab', 'simulink');
