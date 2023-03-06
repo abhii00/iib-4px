2b1d6241d684cf45980ab1015fd707efa63ee5bb
abhii00 <abhijit.pandit808@gmail.com>
2023-03-06 00:24:27 +0000

    fixed instability and updated all models

--- sartoc/controls/taudecomp.m
+++ sartoc/controls/taudecomp.m
@@ -1,20 +1,26 @@
-function tau = lsdecomp(tau_tot, D)
+function tau = taudecomp(tau_tot, D, q_acc)
 %function to decompose overall torque into required reaction wheels torques
 %
 %Arguments:
 %   D (nx3 array): the directions of the n reaction wheels
+%   q_acc (4x1 array): the current quaternion
 %   tau_tot (3x1 array): the total control torque
 %Returns:
 %   tau (nx1 array): the magnitude of the reaction wheel torque
     
+    %convert from q to qm
+    qm_acc = quatconvert(q_acc, 'simulink', 'matlab');
+
+    %rotate D using qm
+    D_acc = rotatepoint(qm_acc, D);
+
     %recast to Ax=b
-    A = D.';
+    A = D_acc.';
     b = tau_tot.';
     
     %solve for x
-    %x = mldivide(A, b);
-    x = pinv(A)*b;
+    x = A\b;
     
-    %recast to origin
+    %recast to original
     tau = x.';
 end
